f1e98e317efe815fcfe085a406833dc7f5d39cda
gsaker <71444105+gsaker@users.noreply.github.com>
2021-05-12 10:52:15 +0100

    Changed many things

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support import expected_conditions as EC
+import functions
 import time
 import sys
+import threading
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+from urllib.request import urlopen
+
+PATH = "/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
 
-#WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "element_id")))
+def checkStock(url):
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("latin-1")
+    if html.find("Sorry, this item is out of stock.") == -1:
+        print("In Stock")
+        return True
+    else:
+        print("Not In Stock")
+        print(url)
+        time.sleep(5)
+        return False
 
-def read_file(step):
+def read_url_file(step):
     count = 0
-    read = open("details.txt","r")
+    read = open("urls.txt","r")
     for i in read:
         print(i)
         if count == step:
@@ -22,92 +38,42 @@ def read_file(step):
         else:
             count += 1
 
-driver.get("https://www.currys.co.uk/gbuk/computing/pc-monitors/pc-monitors/dell-se2219h-full-hd-22-led-monitor-black-10186886-pdt.html")
+def runBuy(url):
+    driver.get(url)
+    functions.acceptCookies(driver)
+    functions.addToBasket(driver)
+    functions.inputAdress(driver)
+    functions.searchAdress(driver)
+    functions.confirmAdress(driver)
+    functions.enterEmail(driver)
+    functions.enterPassword(driver)
+    functions.payButton(driver)
+    functions.enterCCNum(driver)
+    functions.enterCName(driver)
+    functions.enterMM(driver)
+    functions.enterYY(driver)
+    functions.enterCVC(driver)
+    #functions.payConfirm(driver)
+    print("Found in Stock")
+    sys.exit()
 
-try:
-    cookies = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler")))
-    cookies.click()
-except:
-    print("cookie accept failed")
+def mainLoop():
+    urlList = []
+    read = open("urls.txt","r")
+    for line in read:
+        urlList.append(line)
+    print("No of URLS:",len(urlList))
+    for i in range(len(urlList)):
+        print(i)
+        globals()["thread" + str(i)] = threading.Thread(target=checkStockLoop,args=(urlList[i],))
+        globals()["thread" + str(i)].start()
+         
 
-running = True
-    
-while running == True:
-    try:
-        time.sleep(1)
-        basket_add = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='product-actions-touch']/div[4]/div[1]/button")))
-        driver.execute_script("arguments[0].click();", basket_add)
+def checkStockLoop(url):
+    running = True
+    while running == True:
+        if checkStock(url) == True:
+            print("Stock Found")
+            runBuy(url)
 
-        time.sleep(1)
-
-        driver.get("https://www.currys.co.uk/app/checkout")
-
-        running = False
-    except:
-        print("basket add failed")
-
-
-number = 0
-post_code = read_file(number)
-
-address = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='delivery_location']/input"))).send_keys(post_code)
-
-time.sleep(1)
-
-running = True
-while running == True:
-    try:
-        address_select = driver.find_element(By.XPATH, "//*[@id='delivery_location']/button[2]")
-        driver.execute_script("arguments[0].click();", address_select)
-        running = False
-    except:
-        print("address select failed")
-
-running = True
-while running == True:
-    try:
-        address_confirm = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div[2]/div/div[3]/div[1]/button"))).click()
-        running = False
-    except:
-        print("address confirm failed")
-
-number = 1
-mail = read_file(number)
-running = True
-while running == True:
-    try:
-        email = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div[2]/div/div/form/div[1]/div/input").send_keys(mail)
-        running = False
-    except:
-        print("Email input failed")
-
-running = True
-while running == True:
-    try:
-        email_confirm = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div[2]/div/div/form/button")
-        driver.execute_script("arguments[0].click();", email_confirm)
-        running = False
-    except:
-        print("email confirm failed")
-
-
-number = 2
-password = read_file(number)
-
-running = True
-while running == True:
-    try:
-        password_enter = driver.find_element(By.XPATH, "//*[@id='password']/div[1]/input").send_keys(password)
-        password_confirm = driver.find_element(By.XPATH, "//*[@id='password']/div[4]/button").click()
-        running = False
-    except:
-        print("password confirm failed")
-
-running = True
-while running == True:
-    try:
-        pay = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[4]/div[2]/div[2]/div[2]/div[2]/div[1]/button")
-        pay.click()
-        running = False
-    except:
-        print("Go to payment failed")
+mainLoop()
