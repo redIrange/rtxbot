bcaac6cc292b88bab37e184b2c4c0ca497b10640
Oscar Gratton <72939919+redIrange@users.noreply.github.com>
2023-11-08 08:48:14 +0000

    added multithreading to discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,335 +1,187 @@
+from re import S
 import discord
+import time
+import sys
+from pip import main
+import requests
+import threading
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import sys
-import requests
-import threading
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-run = True
+#state var setup and global setup
+global tabs
+global state
+state = 0
 
-PATH = "/Users/meemteam/Desktop/rtxbot/chromedriver"
-caps = DesiredCapabilities().CHROME
-caps["pageLoadStrategy"] = "eager"
-driver = webdriver.Chrome(desired_capabilities=caps, executable_path=PATH)
-
-
-state_change = open("state.txt","w")
-state_change.write("True")
-state_change.close()
-
-
-def check_stock(url):
-  global run
-  print("thread is running")
-  running = True
-  while running == True:
+def read_file(fn,line):
+    # returns line of any file
     temp = []
-    page = requests.get(url)
-    for i in page:
-      temp.append(i)
-    find = str(temp).find("Sorry, this item is out of")
-    find2 = str(temp).find("Out of stock")
-    if find == -1 or find2 == -1:
-      state = open("state.txt","r")
-      for i in state:
-        if i == "True":
-          if run == True:
+    read = open(fn, "r")
+    for i in read:
+        temp.append(i)
+    return(temp[int(line)])
+
+
+def check_stock(url,driver):
+    print("thread " + str(driver) + " is running")
+    run = True
+
+    # loop to check stock continously
+    while run == True and state == 1:
+        time.sleep(1)
+        temp = []
+        page = requests.get(url)
+        for i in page:
+            temp.append(i)
+
+        # checks if item is in stock and calls function to buy
+        find = str(temp).find("Sorry, this item is out of")
+        find2 = str(temp).find("Out of stock")
+        if find == -1 or find2 == -1:
+            # check stock for each website you can buy for
+            if url[12:23] == "endclothing":
+                EndClothing_buy(url, driver)
+            elif url[12:18] == "currys":
+                currys_buy(url, driver)
             run = False
-            buy_gpu(url)
-      state.close()
-    else:
-      print("out of stock")
-
-def buy_gpu(url):
-  #driver_thread = threading.Thread(target=driver_test(url))
-  #driver_thread.start()
-  driver.get(url)
-  basket(driver)
-  address(driver)
-  email(driver)
-  password(driver)
-  pay(driver)
-  pay_card(driver)
-  print("############################### GPU bought ###############################")
-  #message_date.channel.send("@red_range GPU bought")
-  stop_search = open("state.txt","w")
-  stop_search.write("False")
-  stop_search.close()
-
-def driver_test(url):
-  driver.get(url)
+        else:
+            print("out of stock")
 
 def create_threads():
-  urlList = []
-  read = open("urls.txt","r")
-  for line in read:
-    urlList.append(line)
-  print("Nomber of URLS:",len(urlList))
-  range_len = len(urlList)
-  for i in range(len(urlList)):
-    print(i)
-    globals()["thread" + str(i)] = threading.Thread(target=check_stock,args=(urlList[i],))
-    globals()["thread" + str(i)].start()
+    # create threads and chrome windows
 
-def read_file(step):
+    # configure driver
+    cwd = os.getcwd()
+    PATH = cwd + "/chromedriver"
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "eager"
+    service = Service(PATH)
+
+    # configure vars
+    thread_body = {}
+    tabs = {}
     count = 0
-    read = open("details.txt","r")
-    for i in read:
-        if count == step:
-            return i
-        else:
-            count += 1
 
-def basket(driver):
-  count = 0
-  running = True
-  while running == True:
-    try:
-      basket_add = driver.find_element(By.XPATH, "//*[@id='product-actions-touch']/div[4]/div[1]/button")
-      driver.execute_script("arguments[0].click();", basket_add)
-      
-      time.sleep(1)
-      running = False
-      driver.get("https://www.currys.co.uk/app/checkout")
+    # iterate through all urls
+    read = open("urls.txt", "r")
+    for line in read:
+        url = line[0:-3]
+        instances = line[-2:-1]
 
-      #try:
-        #basket_add = driver.find_element(By.XPATH, "//*[@id='product-actions-touch']/div[4]/div[1]/button")
-        #driver.execute_script("arguments[0].click();", basket_add)
+        for i in range(int(instances)):
+        # create thread and chrome windows for each link
+            tabs["{0}".format(count)] = webdriver.Chrome(service=service, desired_capabilities=caps)
+            thread_body["{0}".format(count)] = threading.Thread(target=check_stock, args=(url, tabs["{0}".format(count)]))
+            thread_body[str(count)].start()
 
-      #except:
-        #time.sleep(1)
-        #running = False
-        #driver.get("https://www.currys.co.uk/app/checkout")
+            count = count + 1
 
-    except:
-      #count += 1
+    read.close()
 
-      #if count > 30:
-        #running = False
-      #else:
-      print("basket add failed")
+def test_buy(url, driver):
+    # test buying on oscarg.co.uk
 
-def address(driver):
-  number = 0
-  post_code = read_file(number)
+    driver.get(url)
 
-  running = True
-  while running == True:
-    try:
-      address = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='delivery_location']/input"))).send_keys(post_code)
-      running = False
-    except:
-      print("address input failed")
-
-  time.sleep(2)
-
-  running = True
-  while running == True:
-    try:
-      address_select = driver.find_element(By.XPATH, "//*[@id='delivery_location']/button[2]")
-      driver.execute_script("arguments[0].click();", address_select)
-      running = False
-    except:
-      print("address select failed")
-
-  running = True
-  while running == True:
-    try:
-      address_confirm = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[5]/div[2]/div[2]/div/div[3]/div[1]/button")
-      driver.execute_script("arguments[0].click();", address_confirm)
-      running = False
-    except:
-      running = True
-      while running == True:
+    run = True
+    while run:
         try:
-          address = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='delivery_location']/input"))).send_keys(post_code)
-          running = False
+            checkout = driver.find_element(
+                By.XPATH, "//*[@id='post-39']/div/div/div/a")
+            driver.execute_script("arguments[0].click();", checkout)
+            run = False
         except:
-          print("address input2 failed")
+            print("Button not there")
 
-      time.sleep(6)
-      
-      running = True
-      while running == True:
+
+def currys_buy(url, driver):
+    # buys on currys.co.uk
+    driver.get(url)
+    run = True
+    while run:
+        time.sleep(2)
         try:
-          address_select = driver.find_element(By.XPATH, "//*[@id='delivery_location']/button[2]")
-          driver.execute_script("arguments[0].click();", address_select)
-          running = False
+            checkout = driver.find_element(
+                By.XPATH, "//*[@id='onetrust-accept-btn-handler']")
+            driver.execute_script("arguments[0].click();", checkout)
+            run = False
         except:
-          print("address select failed")
-      
-      running = True
-      while running == True:
-        try:
-          shipping_confirm = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[6]/button")
-          driver.execute_script("arguments[0].click();", shipping_confirm)
-          running = False
-        except:
-          print("idk == true")
-          try:
-            shipping_confirm = driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[5]/div[2]/div[2]/div/div[3]/div[1]/button")
-            driver.execute_script("arguments[0].click();", shipping_confirm)
-            running = False
-          except:
-            print("idk 2 failed")
+            print("Button not there")
 
+def EndClothing_buy(url, driver):
+    # buys on endclothing.com
+    print("endcloting.com")
+    driver.get(url)
 
-def email(driver):
-  number = 1
-  mail = read_file(number)
-  running = True
-  while running == True:
-    try:
-      email = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div[2]/div/div/form/div[1]/div/input").send_keys(mail)
-      running = False
-    except:
-      print("Email input failed")
-
-  running = True
-  while running == True:
-    try:
-      email_confirm = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div[2]/div/div/form/button")
-      driver.execute_script("arguments[0].click();", email_confirm)
-      running = False
-    except:
-      print("email confirm failed")
-
-def password(driver):
-  number = 2
-  password = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      password_enter = driver.find_element(By.XPATH, "//*[@id='password']/div[1]/input").send_keys(password)
-      password_confirm = driver.find_element(By.XPATH, "//*[@id='password']/div[4]/button")
-      driver.execute_script("arguments[0].click();", password_confirm)
-      running = False
-    except:
-      print("password confirm failed")
-
-def pay(driver):
-  running = True
-  while running == True:
-    try:
-      cookie = driver.find_element(By.XPATH, "//*[@id='onetrust-accept-btn-handler']")
-      driver.execute_script("arguments[0].click();", cookie)
-      running = False
-    except:
-      print("Go to payment failed")
-
-  time.sleep(2)
-  running = True
-  while running == True:
-    try:
-      pay = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div/div[4]/div[2]/div[2]/div[2]/div[2]/div[1]/button")
-      driver.execute_script("arguments[0].click();", pay)
-      running = False
-    except:
-      print("Go to payment failed")
-
-def pay_card(driver):
-  number = 3
-  card_number = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      card_enter = driver.find_element(By.ID, "cardNumber").send_keys(card_number)
-      running = False
-    except:
-      print("card number failed")
-
-  number = 4
-  card_number = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      card_enter = driver.find_element(By.ID, "expiryMonth").send_keys(card_number)
-      running = False
-    except:
-      print("card number failed")
-
-  number = 5
-  card_number = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      card_enter = driver.find_element(By.ID, "expiryYear").send_keys(card_number)
-      running = False
-    except:
-      print("card number failed")
-
-  number = 6
-  card_number = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      card_enter = driver.find_element(By.ID, "securityCode").send_keys(card_number)
-      running = False
-    except:
-      print("card number failed")
-
-  number = 7
-  card_number = read_file(number)
-
-  running = True
-  while running == True:
-    try:
-      card_enter = driver.find_element(By.ID, "cardholderName").send_keys(card_number)
-      running = False
-    except:
-      print("card number failed")
-
-  running = True
-  while running:
-    try:
-      pay = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[3]/div[1]/form/fieldset[4]/div/input").click()
-      running = False
-    except:
-      print("Pay Now Failed")
-
-create_threads()
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-
-
-@client.event
-async def on_message(message):
-  global message_date
-  message_date = message
-  if message.content == "!start":
-    print("start")
-    await message.channel.send("Starting GPU search")
-
-    state_change = open("state.txt","w")
-    state_change.write("True")
-    state_change.close()
-
-  elif message.content == "!stop":
-    print("stop")
-    await message.channel.send("Stopping GPU search")
+    run = True
+    while run:
+        time.sleep(10)
+        #try:
+        seize = driver.find_element(By.LINK_TEXT, "UK 10.5")
+        driver.execute_script("arguments[0].click();", seize)
+        run = False
+        #except:
+            #print("failed")
     
-    state_change = open("state.txt","w")
-    state_change.write("False")
-    state_change.close()
+    basket = driver.find.element(By.XPATH, "//*[@id='__page_wrapper']/div/div[1]/div[3]/div/div[4]/div[1]/button/div")
+    driver.execute_script("arguments[0].click();", basket)
 
-  elif message.content == "!exit":
-    print("exiting")
-    await message.channel.send("quiting")
-    sys.exit()
+    
 
+def discord_main(token):
+    # discord remote control function
+    client = discord.Client()
 
-client.run("insert discord token here")
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    @client.event
+    async def on_message(message):
+        global message_date
+        message_date = message
+        if message.content == "!start":
+            print("start")
+            await message.channel.send("Starting search")
+
+            state_change = open("state.txt", "w")
+            state_change.write("True")
+            state_change.close()
+
+        elif message.content == "!stop":
+            print("stop")
+            await message.channel.send("Stopping search")
+
+            state_change = open("state.txt", "w")
+            state_change.write("False")
+            state_change.close()
+
+        elif message.content == "!exit":
+            print("exiting")
+            await message.channel.send("quiting")
+            sys.exit()
+
+    client.run(token)
+
+main_run = True
+while main_run:
+    # control loop
+    menu = int(input("""
+0: Quit
+1: Start
+2: Stop
+    """))
+    if menu == 0:
+        sys.exit()
+    if menu == 1:
+        state = 1
+        create_threads()
+    if menu == 2:
+        state = 0
